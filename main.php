59872b4cabfe519d5cd4847ccb0bd10561444c3c
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-01-19 11:28:00 +0300

    dd

--- main.php
+++ main.php
@@ -4,3 +4,37 @@ Plugin Name: add-url-to-blocks
 Description: Add URL to blocks
 */
 
+namespace AddUrlToBlocks;
+
+add_action('enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_assets');
+// add_filter('register_block_type_args', __NAMESPACE__ . '\chg_args', 10, 2);
+function enqueue_assets()
+{
+    $asset_file = include plugin_dir_path(__FILE__) . 'build/index.asset.php';
+
+    var_dump($asset_file); exit;
+    wp_enqueue_script(
+        'add-url-to-blocks-scripts',
+        plugin_dir_url(__FILE__) . 'build/index.js',
+        $asset_file['dependencies'],
+        $asset_file['version']
+    );
+
+    // wp_set_script_translations(
+    //     'add-url-to-blocks-scripts',
+    //     'add-url-to-blocks'
+    // );
+
+}
+
+function chg_args($args, $block_type)
+{
+
+    if ('core/cover' === $block_type) {
+        $args['attributes']['url'] = array(
+            'type' => 'string',
+        );
+    }
+
+    return $args;
+}
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { __ } from "@wordpress/i18n";
+import { addFilter } from "@wordpress/hooks";
+import { InspectorControls } from "@wordpress/block-editor";
+import {
+  ExternalLink,
+  PanelBody,
+  PanelRow,
+  ToggleControl,
+} from "@wordpress/components";
+
+function addImageInspectorControls(BlockEdit) {
+  return (props) => {
+    const { name, attributes, setAttributes } = props;
+
+    // Early return if the block is not the Image block.
+    if (name !== "core/cover") {
+      return <BlockEdit {...props} />;
+    }
+
+    // Retrieve selected attributes from the block.
+    const { alt, isDecorative } = attributes;
+
+    return (
+      <>
+        <BlockEdit {...props} />
+        <InspectorControls>
+          <PanelBody title={__("Accessibility", "enable-decorative-images")}>
+            <PanelRow>Add settings here...</PanelRow>
+          </PanelBody>
+        </InspectorControls>
+      </>
+    );
+  };
+}
+
+function addAttribute(settings, name) {
+  // Only add the attribute to Image blocks.
+  if (name === "core/cover") {
+    settings.attributes = {
+      ...settings.attributes,
+      isDecorative: {
+        type: "boolean",
+        default: false,
+      },
+    };
+  }
+
+  return settings;
+}
+
+addFilter(
+  "editor.BlockEdit",
+  "example/add-image-inspector-controls",
+  addImageInspectorControls
+);
+
+addFilter(
+  "blocks.registerBlockType",
+  "developer-hours-examples/add-is-decorative-attribute",
+  addAttribute
+);
